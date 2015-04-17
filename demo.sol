7b738db10943197c591ad6d9c5a2f361c7f24b7d
Dan Nilsson <dan.nilsson@king.com>
2015-04-17 20:38:42 +0200

    - aontseuhsnaoteuh plus box

--- demo.sol
+++ demo.sol
@@ -1319,6 +1319,93 @@ function gen_sphere_particles( x : float, y : float, z : float, w : float, h : f
     end
 end
 
+function gen_plusbox_particles( ps : ParticleSystem, mtx : [float] )
+    local p:int = 0;
+    local lines:int = 0;
+    local lp:int = 0;
+    local kq:int = 0;
+    local per:int = 0;
+    while p < 2 do
+	if p == 1 then
+		per = MAX_PARTICLE_COUNT / lines
+	end
+	local a:int = 0
+	while a < 3 do
+		------------
+		local x = 0
+		while x < 4 do
+			local y = 0
+			while y < 4 do
+				local q = 0
+				while q < 3 do
+					local u:[float] = [4:float]
+					local v:[float] = [4:float]
+					if a == 0 then
+						u[0] = float(x)
+						u[1] = float(y)
+						u[2] = float(q)
+						v[0] = float(x)
+						v[1] = float(y)
+						v[2] = float(q + 1)
+					elseif a == 1 then
+						u[0] = float(x)
+						u[1] = float(q)
+						u[2] = float(y)
+						v[0] = float(x)
+						v[1] = float(q + 1)
+						v[2] = float(y)
+					elseif a == 2 then
+						u[0] = float(q)
+						u[1] = float(x)
+						u[2] = float(y)
+						v[0] = float(q + 1)
+						v[1] = float(x)
+						v[2] = float(y)
+					end
+					local corn = 0
+					if (x == 0 and y == 0) then
+						corn = 1
+					elseif (x == 3 and y == 0) then
+						corn = 1
+					elseif (x == 3 and y == 3) then
+						corn = 1
+					elseif (x == 0 and y == 3) then
+						corn = 1
+					end
+
+					if ((q == 0 and corn == 0) or (q == 1 and corn == 1) or (q == 2 and corn == 0)) then
+						if p == 0 then
+							lines = lines + 1
+						else
+							u[3] = 1.0f
+							v[3] = 1.0f
+							local b = 0
+							while b < 3 do
+								u[b] = (u[b] - 1.5f) * 50.0f
+								v[b] = (v[b] - 1.5f) * 50.0f
+								b = b + 1
+							end
+							lines = lines - 1
+							if lines == 0 then
+		                                                gen_points_from_line(vec_mul(mtx, u), vec_mul(mtx, v), MAX_PARTICLE_COUNT - lp, ps, lp);
+		                                        else
+		                                                gen_points_from_line(vec_mul(mtx, u), vec_mul(mtx, v), per, ps, lp);
+		                                        end
+	                                                lp = lp + per
+						end
+					end
+					q = q + 1
+				end
+				y = y + 1
+			end
+			x = x + 1
+		end
+		a = a + 1
+	end
+	p = p + 1
+    end
+end
+
 function update_meshy_cube( ps : ParticleSystem, qb : QuadBatch, delta : float )
 
     if (ps.cool_down > 0.0f) then
@@ -1802,6 +1889,8 @@ function run_floor()
 
 	    local dip_mtx = trans_mtx(sin(psyk_t)*move*200.0f,cos(psyk_t*3.0f) * 200.0f - 50.0f,cos(psyk_t*0.74f)*move*200.0f);
 	    rot_mtx = mtx_mul(dip_mtx, rot_mtx);
+	    
+	    water_t = 1.0f
 
             if water_t > 0.0f then
 
@@ -1814,6 +1903,7 @@ function run_floor()
 		else
                     gen_sphere_particles(0.0f, 0.0f, 10.0f, 100.0f, 100.0f, 100.0f, test_psys, rot_mtx )
                 end
+                    gen_plusbox_particles(test_psys, rot_mtx )
 
 		if do_switch == 1 then
                     if (test_psys.figure == PARTICLE_FIGURE_CUBE) then
